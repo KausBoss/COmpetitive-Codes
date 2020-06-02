2436d7b8ccfed2f3c57b549bfe35a55856bd7612
KausBoss <KausBoss@github.com>
2020-06-02 15:58:56 +0530

    9

--- HOLI_SPOJ.cpp
+++ HOLI_SPOJ.cpp
@@ -17,6 +17,12 @@ using namespace std;
 #define PNF1(a,n,m) for(int i=1;i<=n;i++){for(int j=1;j<=m;j++){cout<<a[i][j]<<' ';}cout<<endl;}cout<<endl;
 const int nax = 1e4+5;
 const int mod = 1e9+7;
+// idead is to maximize use of every edge.
+// an edge can be used a max  2*min(number of nodes in left, number of nodes in right)
+// this is becuase a one traveller from left will occupy on house on right and vice versa
+// hence distace will be dist_of_edge*2*min(number of nodes in left, number of nodes in right)
+
+ll n;
 
 template<typename T>
 class graph{
@@ -26,24 +32,30 @@ public:
 		h[u].pb({v,dist});
 		h[v].pb({u, dist});
 	}
+	// using dfs to count the number of nodes on each side of edge
 	ll dfs(T node, unordered_map<T, bool> &visited, ll *cnt, ll &ans){
 		visited[node]=1;
+		//counting the current node in the count
 		cnt[node] =1;
 
 		for(auto child: h[node]){
+			//dx is the weight of the edge
 			ll dx = child.si;
 			if(!visited[child.fi]){
-				cnt[node] += dfs(child.fi, visited, ans);
+				//counting all nodes on the side of the current node in cnt[node] using dfs
+				cnt[node] += dfs(child.fi, visited, cnt, ans);
 				ll right = cnt[child.fi];
+				// the other side will have n- right number of nodes
 				ll left = n - right;
 				ans += 2*min(left, right)*dx;
 			}
 		}
 		return cnt[node];
 	}
-	void holiday(){
+	ll holiday(){
 		unordered_map<T, bool> visited;
-		ll *cnt = new cnt[n];
+		//cnt[node] stores the number of nodes on the side of node including itself
+		ll *cnt = new ll[n];
 
 		for(ll i=0; i<n; i++){
 			cnt[i] = 0;
@@ -53,24 +65,28 @@ public:
 		return ans;
 	}
 	
-}
+};
 
 int main(){
 	fastIO
 	#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
 	#endif
-	int t;
+
+	ll t,counter=1;
 	cin>>t;
 	while(t--){
-		ll n;
+		graph<ll> g;
 		cin>>n;
-		for(ll i=1; i<n; i++){
-			ll x, y, w;
-			cin>>x>>y>>w;
-			g.addEdge(x-1, y-1, w);
+		for(ll i = 1 ; i<n ; i++){
+			ll x,y,z;
+			cin>>x>>y>>z;
+			g.addEdge(x-1,y-1,z);
 		}
-		cout<<"Case #"<<ctr<<": "<<g.holiday()<<endl;
+		cout<<"Case #"<<counter<<": "<<g.holiday()<<endl;
+		counter++;
 	}
+
+	return 0;
 }
